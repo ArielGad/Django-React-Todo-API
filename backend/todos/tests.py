6f211e2deb245c0d51155d9ebf028b44bd3c5c7b
ArielGad <ariel.gad4@gmail.com>
2019-08-30 17:55:18 +0300

    add tests for title & description fields

--- backend/todos/tests.py
+++ backend/todos/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+class TodoModelsTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='My first todo')
+        Todo.objects.create(description='Practice model test')
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = todo.title
+        self.assertEquals(expected_object_name, 'My first todo')
+
+    def test_description_content(self):
+        todo = Todo.objects.get(id=2)
+        expected_object_description = todo.description
+        self.assertEquals(expected_object_description, 'Practice model test')
